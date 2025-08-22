50a05a10a93f780539e89a8b3538d1f11e00b21d
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-08-22 14:38:27 +0200

    Generalize the function for arbitrary columns

--- code/rank1/open-problems-multimodal/ss_opm/utility/get_group_id.py
+++ code/rank1/open-problems-multimodal/ss_opm/utility/get_group_id.py
@@ -1,18 +1,38 @@
 import numpy as np
+import pandas as pd
 
 
-def get_group_id(metadata):
-    days = [2, 3, 4, 7, 10]
-    donors = [27678, 32606, 13176, 31800]
-    technologies = ["citeseq", "multiome"]
+def get_group_id(metadata, columns=["technology", "donor", "day"]):
+    """Assign group IDs for each row of metadata based on the unique combinations of the values of the specified columns.
+    
+    Parameters
+    ----------
+    metadata : pd.DataFrame
+        The metadata to assign group IDs to.
+    columns : tuple, optional
+        The columns to use to assign group IDs. Default is ("technology", "donor", "day").
+
+    Returns
+    -------
+    group_ids : np.ndarray
+        List of integer group IDs for each row of metadata.
+    """
+
+    assert all(column in metadata.columns for column in columns), f"Some columns of {columns} are not in the metadata"
+
+    group_ids = np.full(len(metadata), fill_value=-1)
+    unique_combinations = metadata[columns].value_counts().index.tolist()
 
     group_id = 0
-    ret = np.full(len(metadata), fill_value=-1)
-    for technology in technologies:
-        for donor in donors:
-            for day in days:
-                selector = (metadata["technology"] == technology) & (metadata["donor"] == donor) & (metadata["day"] == day)
-                ret[selector.values] = group_id
-                group_id += 1
-    assert (ret != -1).all()
-    return ret
+
+    for comb in unique_combinations:
+        selector = pd.Series(True, index=metadata.index)
+        for i, value in enumerate(comb):
+            selector &= (metadata[columns[i]] == value)
+
+        group_ids[selector.values] = group_id
+        group_id += 1
+
+    assert (group_ids != -1).all()
+
+    return group_ids
